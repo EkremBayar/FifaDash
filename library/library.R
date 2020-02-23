e77aee5c3f4d307ccd83e3eeceed32d7bc547d29
EkremBayar <35973642+EkremBayar@users.noreply.github.com>
2020-02-23 23:19:01 +0300

    Add files via upload

---
+++ library/library.R
@@ -0,0 +1,37 @@
+# Description -------------------------------------------------------------
+
+# This R file contains the R libraries.
+
+#shiny libraries
+suppressMessages(library(shiny))
+suppressMessages(library(shinydashboard))
+suppressMessages(library(shinydashboardPlus))
+suppressMessages(library(shinyWidgets))
+suppressMessages(library(shinyalert))
+suppressMessages(library(shinycssloaders))
+suppressMessages(library(shinyjs))
+suppressMessages(library(shinyBS))
+
+#shiny data table & charting libraries
+suppressMessages(library(DT))
+suppressMessages(library(plotly))
+suppressMessages(library(ggdendro))
+suppressMessages(library(factoextra))
+suppressMessages(library(maps))
+
+#tidyverse libraries
+suppressMessages(library(tidyverse))
+suppressMessages(library(stringr))
+suppressMessages(library(magrittr))
+suppressMessages(library(ggrepel))
+suppressMessages(library(gridExtra))
+suppressMessages(library(psych))
+suppressMessages(library(scales))
+suppressMessages(library(ggpubr))
+suppressMessages(library(magick))
+suppressMessages(library(d3heatmap))
+
+suppressMessages(library(rsconnect))
+suppressMessages(library(rmarkdown))
+
+
